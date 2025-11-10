583790786ed2f8e19201800dea7e7f828e764406
kcnsmoothie <kcnsmoothie05@gmail.com>
2025-11-10 13:58:21 +0900

    feat: implement listening socket creation

--- server_socket.cpp
+++ server_socket.cpp
@@ -9,10 +9,17 @@ using namespace std;
 
 int main(){
 
-    //1. Windows 소켓 구조체 생성
+    // 1. Windows 소켓 구조체 생성
     WSADATA wsaData; 
     if(WSAStartup(MAKEWORD(2,2),&wsaData) != 0){
         cout << "WSADATA 생성에 실패했습니다" << endl;
+        return 1;
+    }
+
+    // 2. 리스닝 소켓 생성
+    SOCKET listeningSock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    if(listeningSock = INVALID_SOCKET){
+        cout << "listening socket을 생성할 수 없습니다." << endl;
         return 1;
     }
 }
