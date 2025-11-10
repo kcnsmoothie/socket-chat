6bfd0fdda65041c8fa0a3a851c7659c17264c9f7
kcnsmoothie <kcnsmoothie05@gmail.com>
2025-11-10 17:28:35 +0900

    feat: implement client connection acceptance

--- server_socket.cpp
+++ server_socket.cpp
@@ -5,31 +5,39 @@
 #include <thread>
 using namespace std;
 
-#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib, "ws2_32.lib")
 
-int main(){
+int main()
+{
 
     // 1. Windows 소켓 구조체 생성
-    WSADATA wsaData; 
-    if(WSAStartup(MAKEWORD(2,2),&wsaData) != 0){
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
         cout << "WSADATA 생성에 실패했습니다" << endl;
         return 1;
     }
 
     // 2. 리스닝 소켓 생성
-    SOCKET listeningSock = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
-    if(listeningSock = INVALID_SOCKET){
+    SOCKET listeningSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (listeningSock = INVALID_SOCKET)
+    {
         cout << "listening socket을 생성할 수 없습니다." << endl;
         return 1;
     }
 
     // 3. IP 주소, PORT 번호 바인딩
-    sockaddr_in addrServer {};
+    sockaddr_in addrServer{};
     addrServer.sin_family = AF_INET;
     addrServer.sin_port = htons(4882); // port 번호
-    addrServer.sin_addr.s_addr = htonl(INADDR_ANY); 
+    addrServer.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    bind(listeningSock, (SOCKADDR*)&addrServer, sizeof(addrServer));
-    listen(listeningSock,SOMAXCONN); // 4. 클라이언트 요청 리스닝
+    bind(listeningSock, (SOCKADDR *)&addrServer, sizeof(addrServer));
+    listen(listeningSock, SOMAXCONN); // 4. 클라이언트 요청 리스닝
 
-}
+    // 5. 클라이언트 연결 수락
+    sockaddr_in client = {};
+    int client_size = sizeof(client);
+
+    SOCKET client_socket;
+    client_socket = accept(listeningSock, (SOCKADDR *)&client, &client_size);
